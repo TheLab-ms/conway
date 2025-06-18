67938c3a55ecd08b2eccb791fb2f7b2de1b9b747
Jordan Olshevski <jordanolshevski@gmail.com>
2025-06-18 09:09:24 -0500

    Define metric aggregations at runtime

--- modules/metrics/module.go
+++ modules/metrics/module.go
@@ -19,17 +19,47 @@ func New(db *sql.DB) *Module {
 }
 
 func (m *Module) AttachWorkers(mgr *engine.ProcMgr) {
-	mgr.Add(engine.Poll(time.Second, m.visitAggregates))
+	mgr.Add(engine.Poll(time.Minute, m.visitAggregates))
 }
 
 func (m *Module) visitAggregates(ctx context.Context) bool {
+	aggregates, err := m.getAggregates(ctx)
+	if err != nil {
+		slog.Error("failed to get metric aggregates", "error", err)
+		return false
+	}
+
 	for _, agg := range aggregates {
-		m.aggregate(ctx, agg)
+		m.evalAggregate(ctx, agg)
 	}
 	return false
 }
 
-func (m *Module) aggregate(ctx context.Context, agg *aggregate) bool {
+func (m *Module) getAggregates(ctx context.Context) ([]*aggregate, error) {
+	rows, err := m.db.QueryContext(ctx, `SELECT name, query, interval_seconds FROM metrics_aggregates`)
+	if err != nil {
+		return nil, fmt.Errorf("querying aggregations: %w", err)
+	}
+	defer rows.Close()
+
+	var aggregates []*aggregate
+	for rows.Next() {
+		var agg aggregate
+		var intervalSeconds int64
+		if err := rows.Scan(&agg.Name, &agg.Query, &intervalSeconds); err != nil {
+			return nil, fmt.Errorf("scanning aggregate: %w", err)
+		}
+		agg.Interval = time.Duration(intervalSeconds) * time.Second
+		aggregates = append(aggregates, &agg)
+	}
+	if err := rows.Err(); err != nil {
+		return nil, fmt.Errorf("iterating aggregates: %w", err)
+	}
+
+	return aggregates, nil
+}
+
+func (m *Module) evalAggregate(ctx context.Context, agg *aggregate) bool {
 	var since *float64
 	var start float64
 	err := m.db.QueryRowContext(ctx, "SELECT unixepoch('subsec') - MAX(timestamp), COALESCE(MAX(timestamp), 0.0) FROM metrics WHERE series = $1", agg.Name).Scan(&since, &start)
@@ -38,15 +68,22 @@ func (m *Module) aggregate(ctx context.Context, agg *aggregate) bool {
 		return false
 	}
 	if err == nil && since != nil && *since < agg.Interval.Seconds() {
-		return true
+		return true // not ready to be sampled yet
 	}
 
 	query := fmt.Sprintf("INSERT INTO metrics (series, value) VALUES ($1, (%s))", agg.Query)
 	_, err = m.db.ExecContext(ctx, query, agg.Name, sql.Named("last", int64(start)))
 	if err != nil {
-		slog.Error("failed to insert metric", "metric", agg.Name, "error", err)
+		slog.Error("failed to insert aggregated metric", "metric", agg.Name, "error", err)
 		return false
 	}
+
 	slog.Info("aggregated metric", "metric", agg.Name)
 	return true
+}
+
+type aggregate struct {
+	Name     string
+	Query    string
+	Interval time.Duration
 }
--- modules/metrics/module_test.go
+++ modules/metrics/module_test.go
@@ -1,9 +1,7 @@
 package metrics
 
 import (
-	"context"
 	"testing"
-	"time"
 
 	"github.com/TheLab-ms/conway/db"
 	"github.com/stretchr/testify/assert"
@@ -11,28 +9,17 @@ import (
 )
 
 func TestAggregate(t *testing.T) {
-	ctx := context.Background()
-	db := db.NewTest(t)
-	m := &Module{db: db}
+	testDB := db.NewTest(t)
+	m := &Module{db: testDB}
 
-	// Basics
-	for range 50 {
-		time.Sleep(time.Millisecond)
-		m.aggregate(ctx, &aggregate{
-			Name:     "test",
-			Query:    "SELECT COUNT(*) FROM metrics",
-			Interval: time.Millisecond * 50,
-		})
-	}
-	var count int
-	err := db.QueryRow("SELECT COUNT(*) FROM metrics WHERE series = 'test'").Scan(&count)
+	_, err := testDB.Exec("INSERT INTO metrics_aggregates (name, query, interval_seconds) VALUES ('test-metric', 'SELECT 42', 60)")
 	require.NoError(t, err)
-	assert.Greater(t, count, 1)
-	assert.Less(t, count, 11)
 
-	// Make sure configured aggregates are valid sql
-	for _, agg := range aggregates {
-		assert.True(t, m.aggregate(ctx, agg))
-		assert.True(t, m.aggregate(ctx, agg))
-	}
+	assert.False(t, m.visitAggregates(t.Context()))
+	assert.False(t, m.visitAggregates(t.Context()))
+
+	var count int
+	err = testDB.QueryRow("SELECT COUNT(*) FROM metrics WHERE series = 'test-metric'").Scan(&count)
+	require.NoError(t, err)
+	assert.Equal(t, 1, count)
 }
